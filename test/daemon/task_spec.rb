16802b7e9d81d72cf66a9aebdd4ca238f134a118
Sam de Freyssinet <sam@def.reyssi.net>
2012-01-26 22:09:52 -0600

    Sketching out the tests for the Task Model

---
+++ test/daemon/task_spec.rb
@@ -0,0 +1,48 @@
+# Jenkins Pullover Daemon task test
+
+# Author::    Sam de Freyssinet (sam@def.reyssi.net)
+# Copyright:: Copyright (c) 2012 Sittercity, Inc. All Rights Reserved. 
+# License::   MIT License
+# 
+# Copyright (c) 2012 Sittercity, Inc
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to 
+# deal in the Software without restriction, including without limitation the 
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+# sell copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+# DEALINGS IN THE SOFTWARE.
+
+require_relative "../../lib/github/model.rb"
+require_relative "../../lib/jenkins/model.rb"
+require_relative "../../lib/daemon/task.rb"
+
+describe JenkinsPullover::Daemon::Task do
+
+  it "adds a github model to the task" do
+    
+  end
+
+  it "adds a jenkins model to the task" do
+    
+  end
+
+  it "removes a github model from the task" do
+    
+  end
+
+  it "removes a jenkins model from the task" do
+    
+  end
+end
